ed791a37efbc71b481eb1beb897af5a663c482c7
Enze Zhang <enzezhangss@gmail.com>
2022-07-18 20:19:49 -0400

    Initial commit

---
+++ ApplicationCore/Contracts/Repository/IMovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Contracts.Repository
+{
+    public interface IMovieRepository
+    {
+        List<Movie> GetTop30HighestRevenueMovies();
+
+        List<Movie> GetTop30RatedMovies();
+
+        Movie GetById(int id);
+    }
+}
---
+++ Infrastructure/Services/MovieService.cs
@@ -0,0 +1,27 @@
+﻿using ApplicationCore.Contracts.Services;
+using ApplicationCore.Models;
+using Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class MovieService : IMovieService
+    {
+        public List<MovieCardModel> GetTopRevenueMovies()
+        {
+            var movieRepository = new MovieRepository();
+            var movies = movieRepository.GetTop30HighestRevenueMovies();
+
+            var movieCards = new List<MovieCardModel>();
+            foreach (var movie in movies)
+            {
+                movieCards.Add(new MovieCardModel { Id = movie.Id, Title = movie.Title, PosterUrl = movie.PosterUrl });
+            }
+            return movieCards;
+        }
+    }
+}
